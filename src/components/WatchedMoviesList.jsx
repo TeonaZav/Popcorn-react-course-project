2df28f89e8c62cc906ab987189e952d133761daa
TeonaZav <teona.zavrashvili@gmail.com>
2023-06-27 05:13:13 +0400

    fix bug

--- src/components/WatchedMoviesList.jsx
+++ src/components/WatchedMoviesList.jsx
@@ -3,13 +3,14 @@ import WatchedMovie from "./WatchedMovie";
 const WatchedMoviesList = ({ watched, onDeleteWatched }) => {
   return (
     <ul className="list">
-      {watched.map((movie, i) => (
-        <WatchedMovie
-          movie={movie}
-          key={`${movie.imdbID}/${i}`}
-          onDeleteWatched={onDeleteWatched}
-        />
-      ))}
+      {watched &&
+        watched.map((movie, i) => (
+          <WatchedMovie
+            movie={movie}
+            key={`${movie.imdbID}/${i}`}
+            onDeleteWatched={onDeleteWatched}
+          />
+        ))}
     </ul>
   );
 };
